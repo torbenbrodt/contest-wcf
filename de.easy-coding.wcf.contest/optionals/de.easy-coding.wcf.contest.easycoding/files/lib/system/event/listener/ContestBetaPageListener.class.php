b6dd31a7bcaa172c4c7dec9af8f696c75b783741
torbenbrodt <t.brodt@gmail.com>
2010-01-09 19:44:39 +0000

    * packaged all files and installed the packages at easy-coding  * see it only at http://www.easy-coding.de/index.php?page=ContestOverview  * added notice for beta users

---
+++ de.easy-coding.wcf.contest/optionals/de.easy-coding.wcf.contest.easycoding/files/lib/system/event/listener/ContestBetaPageListener.class.php
@@ -0,0 +1,23 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/system/event/EventListener.class.php');
+
+/**
+ * Show info, that you are a beta user.
+ * 
+ * @author	Torben Brodt
+ * @copyright	2009 TBR Solutions
+ * @license	GNU General Public License <http://opensource.org/licenses/gpl-3.0.html>
+ * @package	de.easy-coding.wcf.contest
+ */
+class ContestBetaPageListener implements EventListener {
+	/**
+	 * @see EventListener::execute()
+	 */
+	public function execute($eventObj, $className, $eventName) {
+		if (MODULE_CONTEST == 1) {
+			WCF::getTPL()->append('userMessages', '<p class="info">Das Contest Plugin ist noch im Beta Stadium. Du kannst diese Seite nur sehen, weil du der <a href="index.php?page=Project&id=contest">Projektgruppe</a> beigetren bist. Mehr Informationen findest du im <a href="http://trac.easy-coding.de/trac/contest">Trac</a>.</p>');
+		}
+	}
+}
+?>
