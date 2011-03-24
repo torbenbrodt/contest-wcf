d6b2d383071f2cb041d041657c61879b7137b6bd
torbenbrodt <t.brodt@gmail.com>
2011-03-24 18:51:44 +0000

    contest interaction example setup

--- de.easy-coding.wcf.contest/optionals/de.easy-coding.wcf.contest.interaction/setup.sql
+++ de.easy-coding.wcf.contest/optionals/de.easy-coding.wcf.contest.interaction/setup.sql
@@ -1,29 +1,15 @@
 -- easy-coding.de example
 UPDATE `wcf1_contest` SET `enableInteraction` = '1' WHERE `contestID` = 3 LIMIT 1;
 
-INSERT INTO `wcf1_contest_interaction_ruleset` (
- `rulesetID` ,
- `kind` ,
- `rulesetTable` ,
- `rulesetColumn` ,
- `rulesetColumnTime` ,
- `rulesetFactor` 
-)
-VALUES (
- NULL , 'user', 'wbb1_1_post', 'userID', 'time', '1'
-), (
- NULL , 'user', 'wcf1_lexicon_item', 'author', 'createTime', '15'
-);
+INSERT INTO `wcf1_contest_interaction` (`interactionID`, `rulesetID`, `contestID`, `fromTime`, `untilTime`, `rulesetFactor`) VALUES
+(1, 1, 3, 1291849200, 1293145199, 1),
+(2, 2, 3, 1291849200, 1293145199, 15),
+(3, 1, 4, 1300921200, 1303682399, 1),
+(4, 2, 4, 1300921200, 1303682399, 10),
+(5, 3, 4, 1300575600, 1303682399, 3);
 
-INSERT INTO `wcf1_contest_interaction` (
- `interactionID` ,
- `rulesetID` ,
- `contestID` ,
- `fromTime` ,
- `untilTime`
-)
-VALUES (
- NULL , '1', '3', UNIX_TIMESTAMP('2010-12-09 00:00:00') , UNIX_TIMESTAMP('2010-12-23 23:59:59')
-), (
- NULL , '2', '3', UNIX_TIMESTAMP('2010-12-09 00:00:00') , UNIX_TIMESTAMP('2010-12-23 23:59:59')
-);
+INSERT INTO `wcf1_contest_interaction_ruleset` (`rulesetID`, `kind`, `rulesetTable`, `rulesetColumn`, `rulesetColumnTime`) VALUES
+(1, 'user', 'wbb1_1_post', 'userID', 'time'),
+(2, 'user', 'wcf1_lexicon_item', 'author', 'createTime'),
+(3, 'user', 'wcf1_user', 'userID', 'registrationDate');
+
