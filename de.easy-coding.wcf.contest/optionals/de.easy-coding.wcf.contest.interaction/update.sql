394f2c6431f5acda76d25dd5244282330f646eb9
torbenbrodt <t.brodt@gmail.com>
2011-04-23 08:02:40 +0000

    release contest 0.9.4

--- de.easy-coding.wcf.contest/optionals/de.easy-coding.wcf.contest.interaction/update.sql
+++ de.easy-coding.wcf.contest/optionals/de.easy-coding.wcf.contest.interaction/update.sql
@@ -1,1 +1,9 @@
+DROP TABLE IF EXISTS wcf1_contest_interaction_extra;
+CREATE TABLE IF NOT EXISTS wcf1_contest_interaction_extra (
+  contestID int(10) unsigned NOT NULL DEFAULT '0',
+  participantID int(10) unsigned NOT NULL DEFAULT '0',
+  score int(10) unsigned NOT NULL DEFAULT '0',
+  INDEX contestID (contestID)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
 ALTER TABLE wcf1_contest_interaction_ruleset CHANGE kind kind ENUM( 'user', 'group', 'participant' ) NOT NULL;
