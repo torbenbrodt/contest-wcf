985d2b69f5c03f8c541f72e52c60ff389257e356
torbenbrodt <t.brodt@gmail.com>
2011-11-27 14:47:50 +0000

    many, many sql mistakes

--- de.easy-coding.wcf.contest/optionals/de.easy-coding.wcf.contest.interaction/install.sql
+++ de.easy-coding.wcf.contest/optionals/de.easy-coding.wcf.contest.interaction/install.sql
@@ -48,14 +48,15 @@ CREATE TABLE IF NOT EXISTS wcf1_contest_coupon (
 	fromTime INT(10) NOT NULL DEFAULT 0,
 	untilTime INT(10) NOT NULL DEFAULT 0,
 	score int(11) NOT NULL DEFAULT '0',
-	state ENUM('single') NOT NULL DEFAULT '',
+	state ENUM('multi', 'single') NOT NULL DEFAULT 'multi',
+	PRIMARY KEY (couponID),
 	INDEX contestID (contestID)
-) ENGINE=MyISAM	DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS wcf1_contest_coupon_participant;
 CREATE TABLE IF NOT EXISTS wcf1_contest_coupon_participant (
 	couponID int(10) unsigned NOT NULL DEFAULT '0',
 	participantID int(10) unsigned NOT NULL DEFAULT '0',
-	INDEX contestID (contestID),
+	UNIQUE coupon_participant (couponID, participantID),
 	INDEX participantID (participantID)
-) ENGINE=MyISAM	DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
